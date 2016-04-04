d89e7308b867a5a8073d9282666ec2f0b5beec1d
andreybolonin <andreybolonin1989@gmail.com>
2016-04-04 11:07:43 +0300

    123

---
+++ web/app.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function() use($app) {
+    return 'Index';
+});
+
+$app->get('/hello/{name}', function($name) use($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
